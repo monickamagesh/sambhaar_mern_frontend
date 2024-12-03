dfc872dfd1d2f6169e2bc0abcaa614b0102225ac
monickamagesh <monickamagesh@gmail.com>
2024-12-03 15:51:40 +0530

    solved error

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,10 @@
 import React, { useEffect } from 'react'
 import HeroSection from '../../components/home/HeroSection'
-import Categories from '../../components/home/categories'
-import HomeCards from '../../components/home/HomeCards'
-import Trending from '../../components/shop/Trending'
 import DealsSection from '../../components/home/DealsSection'
 import PromoBanner from '../../components/home/PromoBanner'
-import Blogs from '../../components/blogs/blogs'
 import Products from '../../components/shop/Products'
 import PromotionSlider from '../../components/home/PromotionalBanner'
+import Categories from '../../components/home/Categories'
 
 const Home = () => {
 
